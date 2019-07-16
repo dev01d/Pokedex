e9e58c6253dfe19cf4b59d960f081ff7a623f747
Jason Behnke <contact@jasonbehnke.io>
2019-07-15 20:58:57 -0700

    Set up info view
    * All info passed to new view
    * Started evolution chain

--- Pokedex/Controller/PokedexController.swift
+++ Pokedex/Controller/PokedexController.swift
@@ -151,6 +151,11 @@ extension PokedexController {
         
         return cell
     }
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = PokemonInfoController()
+        controller.pokemon = inSearchMode ? filteredPokemon[indexPath.row] : pokemon[indexPath.row]
+        navigationController?.pushViewController(controller, animated: true)
+    }
 }
 
 //  MARK: - UICollectionViewDelegateFlowLayout
@@ -173,6 +178,7 @@ extension PokedexController: UICollectionViewDelegateFlowLayout {
 extension PokedexController: PokedexCellDelegate {
     func presentInfoView(withPokemon pokemon: Pokemon) {
         view.addSubview(infoView)
+        infoView.configureViewComponents()
         infoView.delegate = self
         infoView.pokemon = pokemon
         infoView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: view.frame.width - 64, height: 350)
